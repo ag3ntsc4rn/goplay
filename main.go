88d88551d32fa9e87a922ec8300bdcbc8e241f30
Manish Menon <19154062+ag3ntsc4rn@users.noreply.github.com>
2020-11-17 22:04:58 -0500

    Abstract error handling for v1.0.0

--- main.go
+++ main.go
@@ -5,24 +5,52 @@ import (
 	"os"
 )
 
+type writeFile struct {
+	f *os.File
+	err error
+}
+
+func newWriteFile(filename string) *writeFile {
+	f, err := os.Create(filename)
+	return &writeFile{
+		f: f,
+		err: err,
+	} 
+}
+
+func (w *writeFile) writeString(text string){
+	if w.err != nil {
+		return
+	}
+	_, err := io.WriteString(w.f, text)
+	if err != nil {
+		w.err = err
+	}
+}
+
+func (w *writeFile) Close(){
+	if w.err != nil {
+		return
+	}
+	err := w.f.Close()
+	if err != nil {
+		w.err = err
+	}
+}
+
+func (w *writeFile) Err() error {
+	return w.err
+}
+
 func main(){
-	f, err := os.Create("Hello.txt")
-	if err != nil {
-		panic((err))
-	}
-	_, err = io.WriteString(f, "Hello World!")
-	if err != nil {
-		panic((err))
-	}
+	f := newWriteFile("Hello.txt")
+	f.writeString("Hello World!")
+	f.writeString("More text")
+	f.Close()
 
-	_, err = io.WriteString(f, "More Text!")
+	err := f.Err() 
 	if err != nil {
-		panic((err))
-	}
-
-	err = f.Close()
-	if err != nil {
-		panic((err))
+		panic(err)
 	}
 
 }
